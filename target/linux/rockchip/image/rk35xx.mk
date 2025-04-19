7110d637cd311f6174c44275c0f9f1b60d6db445
thxg123 <86899054+thxg123@users.noreply.github.com>
2025-04-19 14:30:49 +0800

    update rk35xx.mk

--- target/linux/rockchip/image/rk35xx.mk
+++ target/linux/rockchip/image/rk35xx.mk
@@ -353,7 +353,7 @@ $(call Device/rk3568)
   DEVICE_MODEL := ALARK35 3500
   DEVICE_DTS := rk3568-alark35-3500
   SUPPORTED_DEVICES += alark35,3500
-  DEVICE_PACKAGES := kmod-nvme kmod-scsi-core kmod-hwmon-pwmfan kmod-thermal kmod-switch-ip17xx
+  DEVICE_PACKAGES := kmod-nvme kmod-scsi-core kmod-hwmon-pwmfan kmod-thermal
 endef
 TARGET_DEVICES += alark35_3500
 
